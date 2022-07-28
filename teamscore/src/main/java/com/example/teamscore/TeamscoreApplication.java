b3fd214c0b65c6220c9cd016c74e6dd96f418a6b
shruthivenkat137 <82895321+shruthivenkat137@users.noreply.github.com>
2022-07-28 12:28:54 +0530

    springapp added

---
+++ teamscore/src/main/java/com/example/teamscore/TeamscoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamscoreApplication.class, args);
+	}
+
+}
